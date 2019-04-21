bcd09844d1dc111bd5884f79045d224e89f76a35
Todd Denlinger <todd dot denlinger at biz>
2019-04-21 16:37:28 -0500

    SvgProject: removed unused method: loadDummyProject

--- Svg to Png/Model/SvgProject.swift
+++ Svg to Png/Model/SvgProject.swift
@@ -60,27 +60,4 @@ class SvgProject: NSDocument {
 
         _atlases.atlases = project.obj.atlases
     }
-
-    //    MARK: - Public Functions
-
-    func loadDummyProject() {
-        let path = "/Users/todddenlinger/Documents/"
-
-        for idx in 0 ..< 3 {
-            var atlas = Atlas()
-            atlas.folder = "\(path)Sprite\(idx + 1).atlas"
-            atlas.defaultWidth = 32 * (idx + 1)
-            atlas.defaultHeight = 36 * (idx + 1)
-
-            for idx2 in 0 ..< 3 {
-                var svgFile = SVGFile()
-                svgFile.filename = "File\(idx2 + 1).svg"
-                //                svgFile.width = 32 * idx2 + 1
-                //                svgFile.height = 36 * idx2 + 1
-                atlas.svgFiles.append(svgFile)
-            }
-
-            prj.atlases.append(atlas)
-        }
-    }
 }
