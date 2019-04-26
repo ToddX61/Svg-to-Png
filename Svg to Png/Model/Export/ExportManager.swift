787b1452c9899419455e70ab4cb7aee6fdbed4d9
Todd Denlinger <todd dot denlinger at biz>
2019-04-26 07:37:47 -0500

    ExportManager: reconstructed to allow running synchronously

--- Svg to Png/Model/Export/ExportManager.swift
+++ Svg to Png/Model/Export/ExportManager.swift
@@ -4,15 +4,19 @@ import Foundation
 class ExportManager {
     // MARK: - Public Functions
 
-    class func export(atlases: [Atlas], completionHander: @escaping (_ ouput: String) -> Void) {
+    class func export(atlases: [Atlas], async: Bool = true, completionHander: @escaping (_ ouput: String) -> Void) {
         for atlas in atlases {
-            for svgFile in atlas.svgFiles {
-                export(atlas: atlas, svgFile: svgFile, completionHander: completionHander)
-            }
+            export(atlas: atlas, completionHander: completionHander)
+        }
+    }
+    
+    class func export(atlas: Atlas, async: Bool = true, completionHander: @escaping (_ ouput: String) -> Void) {
+        for svgFile in atlas.svgFiles {
+            export(atlas: atlas, svgFile: svgFile, completionHander: completionHander)
         }
     }
 
-    class func export(atlas: Atlas, svgFile: SVGFile, completionHander: @escaping (_ ouput: String) -> Void) {
+    class func export(atlas: Atlas, svgFile: SVGFile, async: Bool = true, completionHander: @escaping (_ ouput: String) -> Void) {
         let exportFiles = ExportFile.create(atlas: atlas, svgFile: svgFile)
 
         for exportFile in exportFiles {
@@ -24,34 +28,53 @@ class ExportManager {
 
             var output: String?
             var exception: NSException?
+            
+            if !async {
+                let cmd = ExportManager.buildExportCommand(exportFile: exportFile)
+                performBash(exportFile: exportFile, output: &output, exception: &exception)
+                
+                let result = transformOutput(exportFile: exportFile, command: cmd, output: output, exception: exception)
+                
+                completionHander(result)
+                return
+            }
 
             DispatchQueue.global(qos: .background).async {
                 let cmd = ExportManager.buildExportCommand(exportFile: exportFile)
-
-                SwiftTryCatch.try(
-                    {
-                        output = Bash.execute(commandName: cmd.command, arguments: cmd.arguments) ?? ""
-                    },
-                    catch: { error in print(error); exception = error },
-                    finally: {}
-                )
+                performBash(exportFile: exportFile, output: &output, exception: &exception)
 
                 DispatchQueue.main.async {
-                    guard exception == nil else {
-                        completionHander(exception?.reason ?? "Unknown Error")
-                        return
-                    }
-
-                    var result = output?.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines) ?? ""
-
-                    if result.isEmpty {
-                        result = "\(cmd.command): \(exportFile.outputURL!.path.abbreviatingWithTildeInPath)"
-                    }
-
+                    let result = transformOutput(exportFile: exportFile, command: cmd, output: output, exception: exception)
                     completionHander(result)
                 }
             }
         }
+    }
+    
+    fileprivate class func performBash(exportFile: ExportFile, output: inout String?, exception: inout NSException?) {
+        let cmd = ExportManager.buildExportCommand(exportFile: exportFile)
+        
+        SwiftTryCatch.try(
+        {
+                output = Bash.execute(commandName: cmd.command, arguments: cmd.arguments) ?? ""
+        },
+        catch: { error in print(error); exception = error },
+        finally: {}
+        )
+    }
+    
+    fileprivate class func transformOutput(exportFile: ExportFile, command: ProcessCommand, output: String?, exception: NSException?) -> String {
+        guard exception == nil else {
+            return (exception?.reason ?? "Unknown Error")
+        }
+        
+        var result = output?.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines) ?? ""
+        
+        if result.isEmpty {
+            result = "\(command.command): \(exportFile.outputURL!.path.abbreviatingWithTildeInPath)"
+        }
+        
+        return result
     }
 
     fileprivate class func buildExportCommand(exportFile: ExportFile) -> ProcessCommand {
