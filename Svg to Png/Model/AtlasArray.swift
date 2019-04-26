0f73011f40b9027681488982479041762ac6ae90
Todd Denlinger <todd dot denlinger at biz>
2019-04-26 05:16:54 -0500

    Atlas Array: func indexOf(...) added back in

--- Svg to Png/Model/AtlasArray.swift
+++ Svg to Png/Model/AtlasArray.swift
@@ -2,10 +2,16 @@
 import Foundation
 
 class AtlasArray: CustomStringConvertible {
-    var atlases: [Atlas] 
+    var atlases: [Atlas]
 
     required init() { atlases = [Atlas]() }
     init(atlases: [Atlas]) { self.atlases = atlases }
 
     var description: String { return "\(atlases)" }
+
+    func indexOf(folder: String) -> Int {
+        var project = ProjectCore()
+        project.atlases = atlases
+        return project.indexOf(folder: folder) ?? -1
+    }
 }
